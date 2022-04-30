2757618c95905ff21bdf5ccf05c2702c01b58b9c
Bruno Urbano Rodrigues <brunourb@gmail.com>
2022-04-30 16:25:32 -0300

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    //Agent é o NÓ que vai rodar o job
+    agent any
+
+    //Fases do pipeline
+    stages {
+        
+       stage('Checkout') {
+            steps {
+                script {
+                    echo 'fazer checkout do projeto'
+                }
+            }
+        }
+     }
+}
