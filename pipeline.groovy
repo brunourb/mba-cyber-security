dbb4e92950dfc5ef7ab6a43517ab7920cef85068
Bruno Urbano Rodrigues <brunourb@gmail.com>
2022-04-30 17:37:48 -0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -41,13 +41,43 @@ pipeline {
             }
         }
         
-       stage('deploy to production') {
+        stage('Deploy em Produção') {
             steps {
                 script {
-                    echo 'fazendo o deploy to production do projeto'
+                    def deploymentDelay = input id: 'Deploy', message: 'Deploy to production?', submitter: 'rkivisto,admin', parameters: [choice(choices: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'], description: 'Hours to delay deployment?', name: 'deploymentDelay')]
+                    sleep time: deploymentDelay.toInteger(), unit: 'HOURS'
+                    echo 'Deploy em produção'
+                    
+                    def discordImageSuccess = 'https://www.jenkins.io/images/logos/formal/256.png'
+                    def discordImageError = 'https://www.jenkins.io/images/logos/fire/256.png'
+
+                    def discordDesc =
+                            "Result: ${currentBuild.currentResult}\n" +
+                                    "Project: Nome projeto\n" +
+                                    "Commit: Quem fez commit\n" +
+                                    "Author: Autor do commit\n" +
+                                    "Message: mensagem do changelog ou commit\n" +
+                                    "Duration: ${currentBuild.durationString}"
+
+                                    //Variaveis de ambiente do Jenkins - NOME DO JOB E NÚMERO DO JOB
+                                    def discordFooter = "${env.JOB_BASE_NAME} (#${BUILD_NUMBER})"
+                                    def discordTitle = "${env.JOB_BASE_NAME} (build #${BUILD_NUMBER})"
+                                    def urlWebhook = "https://discord.com/api/webhooks/883733040646483978/1ww2MvJ4oHCKglPFAia1eFpB_2aNpSfjtZS-FOJTsLtDdY0lQFM2Zw_vLLTaDMT2SKLc"
+                                    //def urlWebhook = "https://discord.com/api/webhooks/711712945934958603/tZiZgmNgW_lHleONDiPu5RVM24wbuxFKcpMBDJsY2WxSqjltAz3UCYupqSIE7q0rlmHP"
+
+                    discordSend description: discordDesc,
+                            footer: discordFooter,
+                            link: env.JOB_URL,
+                            result: currentBuild.currentResult,
+                            title: discordTitle,
+                            webhookURL: urlWebhook,
+                            successful: currentBuild.resultIsBetterOrEqualTo('SUCCESS'),
+                            thumbnail: 'SUCCESS'.equals(currentBuild.currentResult) ? discordImageSuccess : discordImageError
+                    
+                    
+                    
                 }
             }
-        }         
-        
+        }
      }
 }
